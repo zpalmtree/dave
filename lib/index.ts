041dacaf609a8af793411c713f44db87d5dcf206
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2020-03-23 19:53:19 +0000

    Percentage sign

--- lib/index.ts
+++ lib/index.ts
@@ -639,7 +639,8 @@ async function chinked(msg: Message, country: string): Promise<void> {
                         .setFooter('Data source: https://www.worldometers.info/coronavirus/');
 
                     if (!isState) {
-                        embed.addField('Percentage of population infected', 100 * (countryData.casesPerOneMillion / 1_000_000));
+                        const percentage = 100 * (countryData.casesPerOneMillion / 1_000_000);
+                        embed.addField('Percentage of population infected', percentage + '%');
                     }
 
                     msg.channel.send(embed);
