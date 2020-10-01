a155aeb25c8d0f66a14e68bfcb0c687413726305
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2020-10-01 22:01:28 +0100

    'Transparent' background

--- lib/Dot.ts
+++ lib/Dot.ts
@@ -231,7 +231,7 @@ export async function renderDotGraph(timespan: number): Promise<[ number, Canvas
     contextShadow.putImageData(imgShadowBuffer, 0, shadowOffset);
     context.putImageData(imgBuffer, 0, 0);
 
-    outContext.fillStyle = '#525252';
+    outContext.fillStyle = '#2F3136';
     outContext.fillRect(0, 0, outCanvas.width, outCanvas.height);
     outContext.drawImage(canvasShadow, 0, 0);
     outContext.drawImage(canvas, 0, shadowOffset);
@@ -258,20 +258,20 @@ const mul_table = [
     332,329,326,323,320,318,315,312,310,307,304,302,299,297,294,292,
     289,287,285,282,280,278,275,273,271,269,267,265,263,261,259
 ];
-   
+
 const shg_table = [
-     9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17, 
-    17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 
+     9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17,
+    17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19,
     19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20,
     20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21,
     21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
-    21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 
+    21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22,
     22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
-    22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 
+    22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23,
     23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
     23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
-    23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 
-    23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 
+    23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+    23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
     24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
     24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
     24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
@@ -283,7 +283,7 @@ class BlurStack {
     public next: any
     constructor() {
         this.a = 0;
-        this.next = null; 
+        this.next = null;
     }
 }
 
@@ -293,14 +293,14 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
     }
 
     radius |= 0;
-            
+
     const div = radius + radius + 1;
     const w4 = width << 2;
     const widthMinus1  = width - 1;
     const heightMinus1 = height - 1;
     const radiusPlus1  = radius + 1;
     const sumFactor = radiusPlus1 * (radiusPlus1 + 1) / 2;
-    
+
     const stackStart = new BlurStack();
     let stack = stackStart;
     let stackEnd: BlurStack = new BlurStack();
@@ -321,7 +321,7 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
 
     let yw = 0;
     let yi = 0;
-    
+
     const mul_sum = mul_table[radius];
     const shg_sum = shg_table[radius];
 
@@ -337,17 +337,17 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
         a_sum = 0;
 
         pa = pixels[yi + 3];
-        
+
         a_out_sum = radiusPlus1 * pa;
         a_sum += sumFactor * pa;
-        
+
         stack = stackStart;
-        
+
         for (let i = 0; i < radiusPlus1; i++) {
             stack.a = pa;
             stack = stack.next;
         }
-        
+
         for (let i = 1; i < radiusPlus1; i++) {
             p = yi + (( widthMinus1 < i ? widthMinus1 : i ) << 2 );
             pa = pixels[p + 3];
@@ -356,7 +356,7 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
             a_in_sum += pa;
             stack = stack.next;
         }
-        
+
         stackIn = stackStart;
         stackOut = stackEnd;
 
@@ -376,7 +376,7 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
 
         yw += width;
     }
-    
+
     for (let x = 0; x < width; x++) {
         a_in_sum = 0;
         a_sum = 0;
@@ -386,26 +386,26 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
         a_out_sum = radiusPlus1 * pa;
         a_sum += sumFactor * pa;
         stack = stackStart;
-        
+
         for(let i = 0; i < radiusPlus1; i++) {
             stack.a = pa;
             stack = stack.next;
         }
-        
+
         let yp = width;
-        
+
         for(let i = 1; i <= radius; i++) {
             yi = ( yp + x ) << 2;
             a_sum += (stack.a = ( pa = pixels[yi+3])) * (radiusPlus1 - i);
             a_in_sum += pa;
             stack = stack.next;
-        
+
             if (i < heightMinus1)
             {
                 yp += width;
             }
         }
-        
+
         yi = x;
         stackIn = stackStart;
         stackOut = stackEnd;
@@ -417,22 +417,22 @@ function stackBlurCanvasAlpha(pixels: any, width: number, height: number, radius
             if (pa > 0) {
                 pa = 255 / pa;
             }
-            
+
             a_sum -= a_out_sum;
             a_out_sum -= stackIn.a;
-            
+
             p = ( x + (( ( p = y + radiusPlus1) < heightMinus1 ? p : heightMinus1 ) * width )) << 2;
-            
+
             a_sum += ( a_in_sum += ( stackIn.a = pixels[p+3]));
-           
+
             stackIn = stackIn.next;
-            
+
             a_out_sum += ( pa = stackOut.a );
-            
+
             a_in_sum -= pa;
-            
+
             stackOut = stackOut.next;
-            
+
             yi += width;
         }
     }
