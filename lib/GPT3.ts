7a32eddba0445f0f0ff6918ff7272bf9ee004146
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2022-06-21 02:36:52 -0400

    Re-enable ai with content filter

--- lib/GPT3.ts
+++ lib/GPT3.ts
@@ -1,7 +1,12 @@
 import { Message, Util } from 'discord.js';
 import { Configuration, OpenAIApi } from 'openai';
+import Filter from 'bad-words';
 
 import { config } from './Config.js';
+import { BannedWords } from './BannedWords.js';
+
+const badWordFilter = new Filter();
+badWordFilter.addWords(...BannedWords);
 
 const configuration = new Configuration({
     apiKey: config.openaiApiKey,
@@ -10,11 +15,20 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const DEFAULT_TEMPERATURE = 0.9;
-const DEFAULT_MAX_TOKENS = 900;
+const DEFAULT_MAX_TOKENS = 250;
 const DEFAULT_AI_MODEL = 'text-davinci-002';
 const DEFAULT_TIMEOUT = 1000 * 30;
 
 export async function handleGPT3(msg: Message, args: string): Promise<void> {
+    const bannedUsers = [
+        '663270358161293343',
+    ];
+
+    if (bannedUsers.includes(msg.author.id)) {
+        await msg.reply(`Sorry, this function has been disabled for your user.`);
+        return;
+    }
+
     const prompt = args.trim();
 
     if (prompt.length === 0) {
@@ -59,6 +73,13 @@ export async function handleGPT3Request(
     temperature: number = DEFAULT_TEMPERATURE,
     user: string = '',
 ) {
+    if (badWordFilter.isProfane(prompt)) {
+        return {
+            result: undefined,
+            error: `Banned search term. Please try a different prompt to avoid the bot breaking the OpenAI terms of service.`,
+        };
+    }
+
     try {
         const completion = await openai.createCompletion({
             model,
