a7518997dc03866761fcb98d6ac8f0a86ee48a0f
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2022-09-11 00:43:45 -0400

    MR fixes

--- lib/CommandImplementations.ts
+++ lib/CommandImplementations.ts
@@ -166,6 +166,7 @@ export async function handleGen3Count(msg: Message, args: string): Promise<void>
     const res = await fetch(url);
 
     const address = args.trim();
+
     if (address !== '' && !isValidSolAddress(address)) {
         replyWithMention(msg, `That does not appear to be a valid Solana wallet address (${address})`);
         return;
@@ -199,14 +200,22 @@ export async function handleGen3Count(msg: Message, args: string): Promise<void>
         gen3Count += Math.floor(eligibleBurns / 3);
         burns += eligibleBurns;
     }
-    burns = 0;
-    if (address !== '') {
-        if (gen3Count === 0) {
-            replyWithMention(msg, `You have ${burns} eligible burns. Every three 3 slugs burnt will get you one generation 3 slug. Burn ${3 - burns} ${burns > 0 ? `more ` : ''}slug${burns <= 1 ? 's' : ''} to get your first generation 3 slug.`);
-        } else { 
-            replyWithMention(msg, `You are currently set to receive ${gen3Count} generation 3 slug${gen3Count > 1 ? 's' : ''}! You have ${burns} eligible burns. Burn ${3 - burns%3} more slug${3 - burns%3 > 1 ? 's' : ''} to get another generation 3 slug!`);
-        }
 
+    if (address !== '') {
+        const burnsForNextSlug = 3 - (burns % 3);
+        const slugStr = burnsForNextSlug === 1 ? 'slug' : 'slugs';
+
+        if (gen3Count === 0) {
+            replyWithMention(
+                msg,
+                `You have ${burns} eligible burn${burns === 1 ? '' : 's'}. Every three slugs burnt will get you one generation 3 slug. Burn ${burnsForNextSlug} ${burns > 0 ? 'more ' : ''}${slugStr} to be eligible for your first generation 3 slug.`
+            );
+        } else {
+            replyWithMention(
+                msg,
+                `You are currently set to receive ${gen3Count} generation 3 slug${gen3Count > 1 ? 's' : ''}! You have ${burns} eligible burns. Burn ${burnsForNextSlug} more ${slugStr} to be eligible for another generation 3 slug.`,
+            );
+        }
     } else {
         replyWithMention(msg, `The current projected Generation 3 slug supply is ${gen3Count}`);
     }
