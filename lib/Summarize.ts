1f65704846b3d8611373274e32deebf92a87395f
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2023-08-30 18:27:54 -0400

    Use less tokens for ids

--- lib/Summarize.ts
+++ lib/Summarize.ts
@@ -16,6 +16,10 @@ const MAX_SUMMARIZE_INPUT_LENGTH = 3000;
 const cachedMessages = new Map<string, CachedMessage[]>;
 
 export async function handleSummarize(msg: Message): Promise<void> {
+    /* This maps a discord message id, e.g. 1146565736538587278 to a shortened id
+     * like 1000, 1001, 3, etc. This saves tokens in the GPT prompt. */
+    const idMap = new Map<string, number>();
+
     const channel = msg.channel.id;
 
     let contentToSummarize = '';
@@ -27,16 +31,32 @@ export async function handleSummarize(msg: Message): Promise<void> {
         return;
     }
 
+    let currentId = 1000;
+
     for (const storedMessage of previousMessages) {
+        idMap.set(storedMessage.id, currentId);
+
         const username = await getUsername(storedMessage.author, msg.guild);
 
-        const id = `[ID# ${storedMessage.id}]`;
+        const id = `[ID#${currentId}]`;
 
-        const reply = storedMessage.reply
-            ? `[Reply to ID# ${storedMessage.reply}]`
-            : '';
+        let reply = '';
+
+        if (storedMessage.reply) {
+            let replyId = idMap.get(storedMessage.reply);
+
+            /* May be a message before bot started listening */
+            if (!replyId) {
+                /* Use a random ID before the first message ID (1000) */
+                replyId = Number(storedMessage.reply.slice(-3));
+            }
+
+            reply = `[Reply to ID#${replyId}]`;
+        }
 
         contentToSummarize += `${id} ${reply} ${username}: ${storedMessage.content}\n`;
+
+        currentId++;
     }
 
     console.log(`Input length: ${contentToSummarize.length}`);
