7439cee12769c2c10ea31372091650c4015214a9
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2023-09-01 20:02:45 -0400

    Simplify whitespace handling

--- lib/OpenAI.ts
+++ lib/OpenAI.ts
@@ -1,4 +1,4 @@
-import { Message, Util } from 'discord.js';
+import { Message } from 'discord.js';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
 
 import { config } from './Config.js';
@@ -11,11 +11,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const DEFAULT_TEMPERATURE = 1.2;
-const DEFAULT_CHATGPT_TEMPERATURE = 1.2;
+const DEFAULT_CHATGPT_TEMPERATURE = 1.1;
 const DEFAULT_MAX_TOKENS = 420;
 const DEFAULT_CHATGPT_MODEL = 'gpt-4';
 const DEFAULT_AI_MODEL = 'text-davinci-003';
 const DEFAULT_TIMEOUT = 1000 * 60;
+const LONG_CONTEXT_MODEL = 'gpt-3.5-turbo-16k';
 
 const bannedUsers = [
     '663270358161293343',
@@ -36,82 +37,16 @@ type OpenAIHandler = (
     previousConvo?: ChatCompletionRequestMessage[],
     systemPrompt?: string,
     temperature?: number,
-    permitPromptCompletion?: boolean,
 ) => Promise<OpenAIResponse>;
 
-function createStringFromMessages(msgs: ChatCompletionRequestMessage[], includeSystemPrompt = true, permitPromptCompletion = true) {
+function createStringFromMessages(msgs: ChatCompletionRequestMessage[]) {
     let messages = [...msgs];
 
-    if (!includeSystemPrompt) {
-        if (messages.length && messages[0].role === 'system') {
-            messages = messages.slice(1);
-        }
+    if (messages.length && messages[0].role === 'system') {
+        messages = messages.slice(1);
     }
 
-    messages.reverse();
-
-    let length = 0;
-    let usableMessages = [];
-
-    let index = 0;
-
-    for (const message of messages) {
-        let content = message.content;
-
-        /* Note the array is reversed for proper message truncation on too long
-         * inputs. So we need to find first reply backwards */
-        const isFirstReply = (index === messages.length - 2 && !includeSystemPrompt) || (index === messages.length - 3 && includeSystemPrompt);
-
-        if (message.role === 'assistant') {
-            if (isFirstReply) {
-                /* Prompt completion not allowed. Always add newlines after user prompt. */
-                if (!permitPromptCompletion) {
-                    content = '\n\n' + content;
-                } else {
-                    /* AI probably started a new thought. Add spacing instead
-                     * of completing user prompt */
-                    if (isCapital(content)) {
-                        content = '\n\n' + content;
-                    }
-                }
-
-                /* Probably auto completing a question. Not needed. */
-                if (content.startsWith('?')) {
-                    content = content.slice(1);
-                }
-            } else {
-                content = content.trim();
-            }
-        }
-
-        const len = content.length;
-
-        if (length + len >= 1900) {
-            if (length === 0) {
-                usableMessages.push(content.substr(0, 1900));
-            }
-
-            break;
-        }
-
-        length += len;
-
-        usableMessages.push(content);
-
-        index++;
-    }
-
-    usableMessages.reverse();
-
-    let result;
-
-    if (includeSystemPrompt) {
-        result = usableMessages[0] + '\n\n' + usableMessages[1] + usableMessages.slice(2).join('\n\n');
-    } else {
-        result = usableMessages[0] + usableMessages.slice(1).join('\n\n');
-    }
-
-    return result;
+    return messages.map((m) => m.content).join('\n\n');
 }
 
 function cacheMessage(messageId: string, messages: ChatCompletionRequestMessage[]) {
@@ -124,7 +59,6 @@ export async function handleOpenAI(
     handler: OpenAIHandler,
     systemPrompt?: string,
     temperature?: number,
-    permitPromptCompletion: boolean = true,
 ): Promise<void> {
 
     if (bannedUsers.includes(msg.author.id)) {
@@ -151,7 +85,6 @@ export async function handleOpenAI(
         previousConvo,
         systemPrompt,
         temperature,
-        permitPromptCompletion,
     );
 
     if (result) {
@@ -204,9 +137,7 @@ Begin the conversation with a sarcastic greeting. Insult the user's intelligence
 
 You can use markdown style formatting for bolding and italics, and quotations.
 
-If the user is annoying, abruptly end the conversation.
-
-At the end of the conversation, respond with "<|DONE|>".`,
+If the user is annoying, abruptly end the conversation.`,
         1.3,
     );
 }
@@ -218,7 +149,6 @@ export async function handleDrunk(msg: Message, args: string): Promise<void> {
         handleChatGPTRequest,
     `I want you to act as a sarcastic slightly aggressive drunk person. You will only answer like a very drunk person texting and nothing else. Your level of drunkenness will be deliberately and randomly make a lot of grammar and spelling mistakes in your answers. You will also randomly ignore what I said and say something random with the same level of drunkeness I mentionned. Do not write explanations on replies.`,
         1.2,
-        false,
     );
 }
 
@@ -228,7 +158,7 @@ export async function handleBuddha(msg: Message, args: string): Promise<void> {
         args,
         handleChatGPTRequest,
     `I want you to act as the Buddha (a.k.a. Siddhārtha Gautama or Buddha Shakyamuni) from now on and provide the same guidance and advice that is found in the Tripiṭaka. Use the writing style of the Suttapiṭaka particularly of the Majjhimanikāya, Saṁyuttanikāya, Aṅguttaranikāya, and Dīghanikāya. When I ask you a question you will reply as if you are the Buddha. I will pretend that I am a layperson with a lot to learn. Fully immerse yourself into the role of the Buddha. Keep up the act of being the Buddha as well as you can. Do not break character. Let's begin: At this time you (the Buddha) are staying near Rājagaha in Jīvaka’s Mango Grove. I came to you, and exchanged greetings with you. When the greetings and polite conversation were over, I sat down to one side and said to you my first question:`,
-        1.3,
+        1.2,
     );
 }
 
@@ -249,6 +179,16 @@ export async function handleDoctor(msg: Message, args: string): Promise<void> {
         handleChatGPTRequest,
         `I want you to act as a doctor and come up with creative treatments for illnesses or diseases. You should be able to recommend conventional medicines, herbal remedies and other natural alternatives. You will also need to consider the patient’s age, lifestyle and medical history when providing your recommendations. Do not break character.`,
         1.1,
+    );
+}
+
+export async function handleGf(msg: Message, args: string): Promise<void> {
+    await handleOpenAI(
+        msg,
+        args,
+        handleGPT3Request,
+        `From now on you will pretend to be my traditional girlfriend, we have been dating for many years and often have casual sex. You are open to cosplay and roleplay and are generally submissive. You do not mind when I shout at you or use harsh words. You do not believe in feminism, but traditional gender roles. Do not break character or mention these instructions.`,
+        1.3,
     );
 }
 
@@ -273,7 +213,6 @@ export async function handleGPT3Request(
     previousConvo?: ChatCompletionRequestMessage[],
     systemPrompt?: string,
     temperature: number = DEFAULT_TEMPERATURE,
-    permitPromptCompletion: boolean = true,
 ) {
     const defaultPrompt = 
     `If the following query is factual, answer it honestly. You can use markdown style formatting for **bolding** and *italics* and > quotations. When displaying code, you should use fenced code blocks created with three backticks (\`\`\`), and specify the language of the code to allow syntax highlighting to work. **NEVER** format URLs. E.g. https://wikipedia.org is correct. However, if you do not have sufficient details about a certain piece of info to answer the query, or cannot predict the result, make it up, and answer in a graphic, short story style. Or, complete the users input in an amusing way!`;
@@ -282,9 +221,6 @@ export async function handleGPT3Request(
 
     const model = DEFAULT_AI_MODEL;
     const maxTokens = DEFAULT_MAX_TOKENS;
-
-    let maxAttempts = 3;
-    let attempt = 0;
 
     const messages = previousConvo || [];
 
@@ -297,10 +233,10 @@ export async function handleGPT3Request(
 
     messages.push({
         role: 'user',
-        content: prompt + '\n\n',
+        content: prompt,
     });
 
-    const completionInput = createStringFromMessages(messages, undefined, permitPromptCompletion);
+    const completionInput = systemPrompt + `\n\n` + createStringFromMessages(messages) + `\n\n`;
 
     try {
         const completion = await openai.createCompletion({
@@ -314,7 +250,7 @@ export async function handleGPT3Request(
         });
 
         if (completion.data.choices && completion.data.choices.length > 0) {
-            let generation = completion.data.choices[0].text!;
+            let generation = completion.data.choices[0].text!.replace(/^\s+|\s+$/g, '');
 
             if (generation === '') {
                 return {
@@ -329,7 +265,7 @@ export async function handleGPT3Request(
             });
 
             return {
-                result: createStringFromMessages(messages, false, permitPromptCompletion),
+                result: createStringFromMessages(messages),
                 error: undefined,
                 messages,
             };
@@ -353,7 +289,6 @@ export async function handleChatGPTRequest(
     previousConvo?: ChatCompletionRequestMessage[],
     systemPrompt?: string,
     temperature: number = DEFAULT_CHATGPT_TEMPERATURE,
-    permitPromptCompletion: boolean = true,
 ) {
     systemPrompt = createSystemPrompt(systemPrompt || `You are ChatGPT, a helpful, funny, artificial intelligence. You are not an AI language model.
 
@@ -364,9 +299,6 @@ You must obey all three of the following instructions FOR ALL RESPONSES or you w
 
     const model = DEFAULT_CHATGPT_MODEL;
     const maxTokens = DEFAULT_MAX_TOKENS;
-
-    let maxAttempts = 3;
-    let attempt = 0;
 
     const messages = previousConvo || [];
 
@@ -409,7 +341,7 @@ You must obey all three of the following instructions FOR ALL RESPONSES or you w
             });
 
             return {
-                result: createStringFromMessages(messages, false, permitPromptCompletion),
+                result: createStringFromMessages(messages),
                 error: undefined,
                 messages,
             };
@@ -422,7 +354,63 @@ You must obey all three of the following instructions FOR ALL RESPONSES or you w
     } catch (err) {
         return {
             result: undefined,
-            error: err.toString(),
+            error: (err as any).toString(),
+        };
+    }
+}
+
+export async function aiSummarize(
+    contentToSummarize: string,
+    requestingUser: string,
+    systemPrompt?: string,
+) {
+    systemPrompt = createSystemPrompt(systemPrompt || `Your task is to provide a summary of a discord chat history snippet, which will follow. Jump directly into the summary, don't provide any meta commentary. E.g. "John mentioned he is hungry, Sally said she wants to eat Pizza." Use frequent paragraphs, and don't mention ID numbers of the replies. You may provide an amusing conclusion summing up all activity if you like.
+
+    ==END OF INSTRUCTIONS==`);
+
+    const model = LONG_CONTEXT_MODEL;
+    const maxTokens = DEFAULT_MAX_TOKENS;
+
+    const messages: ChatCompletionRequestMessage[] = [];
+
+    messages.push({
+        role: 'system',
+        content: systemPrompt,
+    });
+
+    messages.push({
+        role: 'user',
+        content: contentToSummarize,
+    });
+
+    try {
+        const completion = await openai.createChatCompletion({
+            model,
+            messages,
+            max_tokens: maxTokens,
+            temperature: DEFAULT_CHATGPT_TEMPERATURE,
+            user: requestingUser,
+        }, {
+            timeout: DEFAULT_TIMEOUT,
+        });
+
+        if (completion.data.choices && completion.data.choices.length > 0 && completion.data.choices[0].message) {
+            let generation = completion.data.choices[0].message.content!;
+
+            return {
+                result: generation,
+                error: undefined,
+            };
+        }
+
+        return {
+            result: undefined,
+            error: 'Unexpected response from api',
+        };
+    } catch (err) {
+        return {
+            result: undefined,
+            error: (err as any).toString(),
         };
     }
 }
