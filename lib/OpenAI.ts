750ad2e01d5152f2573a7315b06d1fbf9d3ecc62
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2023-11-20 12:39:10 -0500

    V7

--- lib/OpenAI.ts
+++ lib/OpenAI.ts
@@ -604,7 +604,7 @@ export async function handleAIQuote(msg: Message, args: string): Promise<void> {
         systemPrompt,
         undefined,
         undefined,
-        'ft:gpt-3.5-turbo-1106:personal:fit-quote-bot-v3:8Ms0r161',
+        'ft:gpt-3.5-turbo-1106:personal:fit-quote-bot-v7:8N2T9c3O',
     );
 
     if (result) {
