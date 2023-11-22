913fc19e6f00105b10416510acff9fe078796112
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2023-11-21 22:31:58 -0500

    v19

--- lib/OpenAI.ts
+++ lib/OpenAI.ts
@@ -605,7 +605,7 @@ export async function handleAIQuote(msg: Message, args: string): Promise<void> {
         systemPrompt,
         undefined,
         undefined,
-        'ft:gpt-3.5-turbo-1106:personal:fit-quote-bot-v17:8NSpO14A',
+        'ft:gpt-3.5-turbo-1106:personal:fit-quote-bot-v19:8NYAVNzk',
     );
 
     if (result) {
