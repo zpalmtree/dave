a7e8b7a23bbfe3bfa2a6f6be45033ded7639c67b
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2024-05-14 13:14:13 -0400

    Add transcription

--- lib/index.ts
+++ lib/index.ts
@@ -45,6 +45,7 @@ import {
 import { restoreTimers } from './Timer.js';
 import { cacheMessageForSummarization } from './Summarize.js';
 import { convertTwitterLinks } from './ConvertTwitterLinks.js';
+import { handleTranscribe } from './OpenAI.js';
 
 /* This is the main entry point to handling messages. */
 async function handleMessage(msg: Message, db: sqlite3.Database): Promise<void> {
@@ -62,6 +63,7 @@ async function handleMessage(msg: Message, db: sqlite3.Database): Promise<void> 
         }
 
         convertTwitterLinks(msg);
+        handleTranscribe(msg);
 
         return;
     }
