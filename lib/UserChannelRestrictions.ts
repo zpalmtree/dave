8dade5d261e69ea65024c6b4d95f44d575a27ad7
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2026-04-01 18:01:13 -0400

    Add more blocked users

--- lib/UserChannelRestrictions.ts
+++ lib/UserChannelRestrictions.ts
@@ -12,4 +12,12 @@ export const userChannelRestrictions: UserChannelRestriction[] = [
         userId: '673794444579045386',
         allowedChannels: ['1234575197114204202', '1073613902706909214'],
     },
+    {
+        userId: '1232469668548055061',
+        allowedChannels: ['1234575197114204202', '1073613902706909214'],
+    },
+    {
+        userId: '1259332105188671509',
+        allowedChannels: ['1234575197114204202', '1073613902706909214'],
+    },
 ];
