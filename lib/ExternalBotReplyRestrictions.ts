a0280b9d467a6cc85d46e9ba0f7a2cb514d6bd1c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2026-04-01 18:01:13 -0400

    Add more blocked users

--- lib/ExternalBotReplyRestrictions.ts
+++ lib/ExternalBotReplyRestrictions.ts
@@ -6,7 +6,12 @@ export interface ExternalBotReplyRestriction {
 
 export const externalBotReplyRestrictions: ExternalBotReplyRestriction[] = [
     {
-        blockedUserIds: ['1307359331724824744', '673794444579045386'],
+        blockedUserIds: [
+            '1307359331724824744',
+            '673794444579045386',
+            '1232469668548055061',
+            '1259332105188671509',
+        ],
         botUserId: '723993425325719619',
         triggerPattern: /^\s*fc(?:\s|$)/i,
     },
